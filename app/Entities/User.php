6e25cdfe225c583fc12a68c6e42a1f5eb946495b
Marques <marquesde2013@gmail.com>
2018-04-18 16:50:21 -0300

    Commit Configurando Prettus vs.0.2

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -2,13 +2,22 @@
 
 namespace App\Entities;
 
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
 
+/**
+ * Class User.
+ *
+ * @package namespace App\Entities;
+ */
+class User extends Authenticatable implements Transformable
+{
+    use TransformableTrait;
+    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
@@ -17,12 +26,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
