6a9d86e39545a670aa9a93f1750744cefa9b2dd6
Marques <marquesde2013@gmail.com>
2018-04-19 15:23:20 -0300

    Commit Configurando Validation vs.0.4

--- app/Validators/UserTypeUserValidator.php
+++ app/Validators/UserTypeUserValidator.php
@@ -18,7 +18,11 @@ class UserTypeUserValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'status' => 'required',
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'status' => 'required',
+        ],
     ];
 }
