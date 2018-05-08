fbdb59c98365ba19de1335983efa75144e34ac53
guilh <guilhermedude.7@gmail.com>
2018-05-07 22:13:01 -0300

    Configuração de Database - Factory, Migrate e Seed

--- database/migrations/2018_04_19_001522_create_user_type_users_table.php
+++ database/migrations/2018_04_19_001522_create_user_type_users_table.php
@@ -20,7 +20,7 @@ class CreateUserTypeUsersTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('user_type_id')->unsigned();
             $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('cascade');
-           // $table->boolean('status');
+            $table->boolean('status');
             $table->timestamps();
 		});
 	}
