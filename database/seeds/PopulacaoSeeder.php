4cef09d75db1401c8a3084a212efe87b2f4c2ed2
guilh <guilhermedude.7@gmail.com>
2018-05-12 16:54:07 -0300

    Seed com dados Reais: Evento, Atividade, Tipo de Atividade, Instituição,

--- database/seeds/PopulacaoSeeder.php
+++ database/seeds/PopulacaoSeeder.php
@@ -24,6 +24,7 @@ class PopulacaoSeeder extends Seeder
 
         ]);
 
+        /*---------------Tipos de Usuários---------------------------*/
         \App\Entities\UserType::create([
             'nome' => 'Admisnistrador',
             'descricao' => 'Admisnistrador',
@@ -40,11 +41,119 @@ class PopulacaoSeeder extends Seeder
             'status' => '1',
         ]);
 
+        /*---------------------------------------------------------------*/
         factory(App\Entities\UserTypeUser::class, 5)->create();
-        factory(App\Entities\Institution::class, 5)->create();
-        factory(App\Entities\TypeActivity::class, 5)->create();
+
+        /*--------------------------Instituição-------------------------------*/
+        App\Entities\Institution::create([
+            'nome' => 'IFTO - Campus Paraíso',
+            'descricao' => 'Instituição de nível médio e superior',
+            'site' => 'portal.ifto.edu.br/paraiso',
+            'email' => 'institucional.paraiso@ifto.edu.br',
+            'telefone' => '(63)33610300',
+            'status' => '1',
+        ]);
+        App\Entities\Institution::create([
+            'nome' => 'IFTO - Campus Palmas',
+            'descricao' => 'Instituição de nível médio e superior',
+            'site' => 'portal.ifto.edu.br/paalmas',
+            'email' => 'institucional.palmas@ifto.edu.br',
+            'telefone' => '(63)32150305',
+            'status' => '1',
+        ]);
+
+        /*---------------------------Tipo de Atividade------------------------------------*/
+        App\Entities\TypeActivity::create([
+            'nome' => 'Palestra',
+            'descricao' => 'Palestra',
+            'status' => '1',
+        ]);
+        App\Entities\TypeActivity::create([
+            'nome' => 'Maratona de Programação',
+            'descricao' => 'Discursões de determinados temas com os participantes do evento',
+            'status' => '1',
+        ]);
+        App\Entities\TypeActivity::create([
+            'nome' => 'Minicurso',
+            'descricao' => 'Cursos de curta duração para iniciantes',
+            'status' => '1',
+        ]);
+
+        /*---------------------------------------------------------------*/
         factory(App\Entities\UserActivityType::class, 5)->create();
-        factory(App\Entities\Event::class, 5)->create();
-        factory(App\Entities\Activity::class, 5)->create();
+
+        /*-----------------------------Eventos----------------------------------*/
+        \App\Entities\Event::create([
+            'nome' => 'VI JTGTI',
+            'descricao' => 'Evento de Tecnologia da Informação do IFTO',
+            'local' => 'IFTO - Campus Paraíso do Tocantins - TO',
+            'data_inicio' => '2018-05-08 19:00:00',
+            'data_conclusao' => '2018-05-10 22:00:00',
+            'situacao' => '1',
+            'status' => '1',
+            'institutions_id'=> '1',
+            'coordenador' => 'Fábio Vidal',
+        ]);
+        \App\Entities\Event::create([
+            'nome' => 'V SEMAD',
+            'descricao' => 'Evento de Administração do IFTO',
+            'local' => 'IFTO - Campus Paraíso do Tocantins - TO',
+            'data_inicio' => '2018-06-08 14:00:00',
+            'data_conclusao' => '2018-06-10 18:00:00',
+            'situacao' => '1',
+            'status' => '1',
+            'institutions_id'=> '1',
+            'coordenador' => 'Márcia',
+        ]);
+        \App\Entities\Event::create([
+            'nome' => 'HACKATON',
+            'descricao' => 'Evento de Programação do IFTO',
+            'local' => 'IFTO - Campus Palmas - TO',
+            'data_inicio' => '2018-09-07 14:00:00',
+            'data_conclusao' => '2018-09-09 18:00:00',
+            'situacao' => '1',
+            'status' => '1',
+            'institutions_id'=> '2',
+            'coordenador' => 'Gilberto',
+        ]);
+
+        /*-----------------------------Atividades----------------------------------*/
+        App\Entities\Activity::create([
+            'nome' => 'Minicurso Robótica - Robô Lego',
+            'descricao' => 'Introdução a robótica utilizando robô de lego',
+            'local' => 'IFTO - Campus Paraíso do Tocantins - TO',
+            'data_inicio' => '2018-06-09 14:00:00',
+            'data_conclusao' => '2018-06-08 18:00:00',
+            'horas' => '04:00:00',
+            'qtd_inscritos' => '100',
+            'status' => '1',
+            'type_activities_id'=> '3',
+            'events_id' => '1',
+        ]);
+        App\Entities\Activity::create([
+            'nome' => 'Como Empreender Com Sucesso',
+            'descricao' => 'Palestra sobre o que saber para empreender com sucesso',
+            'local' => 'IFTO - Campus Paraíso do Tocantins - TO',
+            'data_inicio' => '2018-06-08 14:00:00',
+            'data_conclusao' => '2018-06-10 18:00:00',
+            'horas' => '04:00:00',
+            'qtd_inscritos' => '100',
+            'status' => '1',
+            'type_activities_id'=> '1',
+            'events_id' => '2',
+        ]);
+        App\Entities\Activity::create([
+            'nome' => 'Maratona de Programação',
+            'descricao' => '24 horas de programação direta',
+            'local' => 'IFTO - Campus Palmas - TO',
+            'data_inicio' => '2018-09-07 19:00:00',
+            'data_conclusao' => '2018-09-09 19:00:00',
+            'horas' => '24:00:00',
+            'qtd_inscritos' => '50',
+            'status' => '1',
+            'type_activities_id'=> '2',
+            'events_id' => '3',
+        ]);
+        /*---------------------------------------------------------------*/
     }
 }
