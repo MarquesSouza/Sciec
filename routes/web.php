f56abad5472ea5c22ee18bb4f38e6a8e4ebea705
Marques <marquesde2013@gmail.com>
2018-06-08 15:32:08 -0300

    remoção da barra lateral

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', 'EventsController@index')->name('painel');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'EventsController@index')->name('home');
 
 Route::get('token', function (){
     $http = new GuzzleHttp\Client;
