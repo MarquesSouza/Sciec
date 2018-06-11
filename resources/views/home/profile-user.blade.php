5c4f0c1c3f7af14299b1a288897cbad77baf1b09
Gerverson <gerversonsa@gmail.com>
2018-06-11 14:46:43 -0300

    Tela de perfil usuário bug 01

--- resources/views/home/profile-user.blade.php
+++ resources/views/home/profile-user.blade.php
@@ -49,7 +49,7 @@
 								</div>
 								<div class="row">
 									<div class="col-md-4 col-md-offset-6">
-										<div class="col-md-2 text-center"><a href="<{{$user->nome}}>/mestre?acao=editar" class="btn btn-primary" role="button">Editar Informações</a></div>
+										<div class="col-md-2 text-center"><a href="#" class="btn btn-primary" role="button">Editar Informações</a></div>
 									</div>
 								</div>
 							</div>
